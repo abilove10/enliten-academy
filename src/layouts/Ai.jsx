b18e4aba077da995fa43682c5507ec1ab37af4b0
GokulaKrishnan <abirami20113@gmail.com>
2025-02-21 13:52:58 +0530

    Update Ai.jsx

--- src/layouts/Ai.jsx
+++ src/layouts/Ai.jsx
@@ -124,7 +124,7 @@ function Ai(props) {
         //   console.error(error);
         // });
 
-        axios.post("http://127.0.0.1:5000/chat",
+        axios.post("https://api.enliten.org.in/chat",
             { message: q },
             {
             method: 'POST',
